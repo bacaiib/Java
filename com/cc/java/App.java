84daeba5087f91618f6a413e1a2596a1fdc57f6f
bacaiib <roman-frommhold@gmx.net>
2024-01-23 12:50:54 +0100

    Hello World ++

--- com/cc/java/App.java
+++ com/cc/java/App.java
@@ -2,11 +2,7 @@ package com.cc.java;
 
 public class App 
 {
-
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        System.out.println("null");
     }
-    
 }
-
-
